7cbf1c2e18d032b6aa6e151ce919df417d05479b
RE000 <re.paterson000@gmail.com>
2020-06-05 09:03:09 +1000

    added live time

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Current day and time
-$(".timeDate").text(moment().format('LLLL'));
+$(".timeDateTwo").text(moment().format('LL'));
+$(".timeDate").text(moment().format('LTS'));
+setInterval(function(){
+    $(".timeDate").text(moment().format('LTS'));
+},1000);
+
 
 // Work day hours
 var hours = ["6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM"];
@@ -12,11 +17,13 @@ for (i = 0; i < hours.length; i++) {
 
     time = $("<div>");
     time.addClass("input-group-prepend");
+    time.css("color", "grey");
 
     timeText = $("<span>");
     timeText.addClass("input-group-text");
     timeText.text(hours[i]);
     timeText.css("width", "4.5rem");
+    timeText.attr("data-index");
 
     userInput = $("<input>");
     userInput.addClass("form-control");
@@ -26,7 +33,7 @@ for (i = 0; i < hours.length; i++) {
     UserInputAppend.addClass("input-group-append");
 
     saveButton = $("<button>");
-    saveButton.addClass("fa fa-floppy-o btn btn-outline-secondary");
+    saveButton.addClass("fa fa-floppy-o btn btn-secondary");
 
     UserInputAppend.append(saveButton);
     time.append(timeText);
