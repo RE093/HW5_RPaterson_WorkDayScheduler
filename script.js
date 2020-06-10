2b7b561c9bf583e9b94ca487c1e904ddf7c4fb52
RE000 <re.paterson000@gmail.com>
2020-06-10 14:49:17 +1000

    added clear

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 
     // Work day hours
     var hours = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM"];
-    var value = ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+    var value = ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
     var time = "";
     var taskArray = [];
     
@@ -31,7 +31,7 @@ $(document).ready(function () {
         var timeText = $("<span>");
         timeText.addClass("input-group-text");
         timeText.text(hours[i]);
-        timeText.attr("value", value[i])
+        timeText.attr("value", value[i]);
         timeText.css("width", "4.5rem");
 
         var userInput = $("<div>");
@@ -39,26 +39,23 @@ $(document).ready(function () {
         userInput.text(taskArray[i]);
         userInput.attr("contentEditable", "true");
 
-        var UserInputAppend = $("<div>");
-        UserInputAppend.addClass("input-group-append");
+        var userInputAppend = $("<div>");
+        userInputAppend.addClass("input-group-append");
 
         var saveButton = $("<button>");
-        saveButton.addClass("fa fa-floppy-o btn btn-secondary save");
+        saveButton.addClass("fa fa-floppy-o btn btn-primary save");
         saveButton.data("index", i);
 
-        UserInputAppend.append(saveButton);
+        userInputAppend.append(saveButton);
         time.append(timeText);
         hourBlocks.append(time);
         hourBlocks.append(userInput);
-        hourBlocks.append(UserInputAppend);
+        hourBlocks.append(userInputAppend);
         $(".container").append(hourBlocks);
 
         // Changing div color based on current time
-        var time = moment().format('HH')
-        var timeIndex = timeText.attr("value")
-
-        console.log(time)
-        console.log(timeIndex)
+        var time = moment().format('HH');
+        var timeIndex = timeText.attr("value");
 
         if (time > timeText.attr("value")) {
             timeText.css("background-color", "lightgrey")
@@ -69,27 +66,33 @@ $(document).ready(function () {
         if (time < timeIndex) {
             timeText.css("background-color", "lightgreen")
         }
-        if (time > 20 && time < 24) {
-            timeText.css("background-color", "lightgreen")
-        }
     };
 
     // store information locally
     $(".save").on("click", function () {
-            var task = $(this).parents(".hourBlock");
-            var taskIndex = task.data("index");
-            var userInputFind = task.find(".userInput");
-                
-            localStorage.setItem("task" + taskIndex, JSON.stringify(userInputFind.text()));
+        var task = $(this).parents(".hourBlock");
+        var taskIndex = task.data("index");
+        var userInputFind = task.find(".userInput");
+            
+        localStorage.setItem("task" + taskIndex, JSON.stringify(userInputFind.text()));
     });
 
 
     // get local information
     function getTasks() {
-        
+    
         for (i = 0; i < hours.length; i++) {
-            var gotten = JSON.parse(localStorage.getItem("task" + i,)) 
-            taskArray.push(gotten)
+            var items = JSON.parse(localStorage.getItem("task" + i,)) 
+            taskArray.push(items)
         };
+        console.log(taskArray)
     };
+
+    // Clear all button
+    var clear = $(".clear");
+    clear.on("click", function() {
+        for (i = 0; i < 15; i++) {
+            localStorage.setItem("task" + i, JSON.stringify(""));
+        };
+    });
 });
